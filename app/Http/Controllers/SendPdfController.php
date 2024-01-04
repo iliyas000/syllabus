d7d2ebb99bb6dd34f9094285d523f19137b474bc
ВашÐiliyas000 <_aliyakparoff@gmail.com>
2024-01-04 10:11:59 +0000

    gg

---
+++ app/Http/Controllers/SendPdfController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\DB;
+
+class SendPdfController extends Controller
+{
+    public function SendPdf($category, $file_id)
+    {
+        if($category == 'syllabus'){
+            $file_url = DB::table('document_to_log')
+                ->where('file_name', 'syllabus_'.$file_id)
+                ->orderByDesc('id')
+                ->select('file_url')
+                ->first();
+        }
+        $b64Doc = chunk_split(base64_encode(file_get_contents($file_url->file_url)));
+        return $b64Doc;
+    }
+
+
+}
